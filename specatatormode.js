051fc24312e229af2c113a7256702b114186c046
Sam Wilber <samjwilber@gmail.com>
2015-03-26 00:50:17 -0400

    Spectator Mode
    Don’t accidentally join games

---
+++ specatatormode.js
@@ -0,0 +1,30 @@
+// ==UserScript==
+// @name          Specatator Mode
+// @namespace     http://reddit.com/u/samwilber
+// @description   Never accidentally join a game again
+// @include       http://tangent.jukejuice.com*
+// @include       http://tagpro-*.koalabeast.com*
+// @author        turtlemansam and help from bizkut's script
+// @version       1.0
+// @grant         GM_getValue
+// @grant         GM_setValue
+// ==/UserScript==
+
+$(document).ready(function(){
+    $('article div.section.smaller:eq(0)').append("<input type='checkbox' id='tglSpec'>Spectator Mode</input>");
+    if (GM_getValue("specMode") === true) {
+    $("#tglSpec").prop('checked', true);
+    }
+    $("#tglSpec").on('change', function () {
+    if ($(this).is(":checked")) {
+        GM_setValue("specMode", true)
+    } else {
+        GM_setValue("specMode", false)
+    }
+    })
+    if (GM_getValue("specMode") === true) {
+        if(document.URL.search('games/find')>=0) {
+        window.location.href = "/";
+        }
+    }
+});
