638b674bf264b07b37ade4f959ab952207f967af
Sam Wilber <samjwilber@gmail.com>
2015-04-02 13:35:19 -0400

    Update specatatormode.user.js

--- specatatormode.user.js
+++ specatatormode.user.js
@@ -5,23 +5,25 @@
 // @include       http://tangent.jukejuice.com*
 // @include       http://tagpro-*.koalabeast.com*
 // @author        turtlemansam and help from bizkut's script
-// @version       1.0
+// @version       1.2
 // @grant         GM_getValue
 // @grant         GM_setValue
 // ==/UserScript==
 
 $(document).ready(function(){
-    $('article div.section.smaller:eq(0)').append("<input type='checkbox' id='tglSpec'>Spectator Mode</input>");
+    if(document.URL.endsWith(".com/") === true) {
+        $('article div.section.smaller:eq(0)').append("<input type='checkbox' id='tglSpec'>Spectator Mode</input>");
+    };
     if (GM_getValue("specMode") === true) {
-    $("#tglSpec").prop('checked', true);
+        $("#tglSpec").prop('checked', true);
     }
     $("#tglSpec").on('change', function () {
-    if ($(this).is(":checked")) {
-        GM_setValue("specMode", true)
-    } else {
-        GM_setValue("specMode", false)
-    }
-    })
+        if ($(this).is(":checked")) {
+            GM_setValue("specMode", true)
+        } else {
+            GM_setValue("specMode", false)
+        }
+    });
     if (GM_getValue("specMode") === true) {
         if(document.URL.search('games/find')>=0) {
         window.location.href = "/";
